4068751e666d4803864bc4ed0df720c4fe9dbb2e
Takasur Azeem <takasurazeem@gmail.com>
2023-07-30 21:26:07 +0500

    Initial Commit

---
+++ QuranQuiz/QuranQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuranQuizApp.swift
+//  QuranQuiz
+//
+//  Created by Takasur Azeem on 30/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct QuranQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
