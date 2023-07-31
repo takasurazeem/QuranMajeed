b7de3704c173bc6ae47283cce47f32245171a87f
Takasur Azeem <takasurazeem@gmail.com>
2023-08-01 01:41:52 +0500

    Code cleanup
    Signed-off-by: Takasur Azeem <takasurazeem@gmail.com>

--- QuranQuiz/Quiz/PDFPreview/View/QuizPDFPreview.swift
+++ QuranQuiz/Quiz/PDFPreview/View/QuizPDFPreview.swift
@@ -29,87 +29,14 @@ struct QuizPDFPreview: View {
     }
 }
 
-// TODO: - Set custom border
-//class BorderAnnotation: PDFBorder {
-//    override func draw(in rect: CGRect) {
-//        super.draw(in: rect)
-//        style = .dashed
-//    }
-//}
-//
-//class MainPDFViewDelegate: NSObject, PDFDocumentDelegate {
-//
-//    func `class`(forAnnotationType annotationType: String) -> AnyClass {
-//        BorderAnnotation.self
-//    }
-//}
-
-// Add this:
-struct PDFKitView: UIViewRepresentable {
-    public var documentData: Data?
-//    let documentDelegate = MainPDFViewDelegate()
-    
-    func makeUIView(context: UIViewRepresentableContext<PDFKitView>) -> PDFView {
-        // Creating a new PDFVIew and adding a document to it
-        let pdfView = PDFView()
-        if let documentData {
-            let document = PDFDocument(data: documentData)!
-            pdfView.document = document
-            let bounds = pdfView.document!.page(at: document.pageCount - 1)!.bounds(for: .mediaBox).size
-            let annotation = PDFAnnotation(
-                bounds: CGRect(
-                    x: 0,
-                    y: 0,
-                    width: bounds.width,
-                    height: bounds.height
-                ),
-                forType: .square,
-                withProperties: nil
-            )
-            
-            let border = PDFBorder()
-            border.style = .dashed
-            border.lineWidth = 3
-            
-            annotation.border = border
-            let lastPage = document.page(at: document.pageCount - 1)
-            lastPage?.addAnnotation(annotation)
-            // TODO: - Set custom border
-//            document.delegate = documentDelegate
-            pdfView.autoScales = true
-        }
-
-        return pdfView
-    }
-    
-    func updateUIView(_ uiView: PDFView, context: UIViewRepresentableContext<PDFKitView>) {
-        // TODO
-    }
-}
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        QuizPDFPreview(documentData: PDFCreator(title: "Test", verses: []).createFlyer())
+        QuizPDFPreview(
+            documentData: PDFCreator(
+                title: "Quiz",
+                verses: []
+            )
+            .createFlyer()
+        )
     }
-}
-
-extension PDFDocument: Transferable {
-    public static var transferRepresentation: some TransferRepresentation {
-        DataRepresentation(contentType: .pdf) { pdf in
-                if let data = pdf.dataRepresentation() {
-                    return data
-                } else {
-                    return Data()
-                }
-            } importing: { data in
-                if let pdf = PDFDocument(data: data) {
-                    return pdf
-                } else {
-                    return PDFDocument()
-                }
-            }
-        DataRepresentation(exportedContentType: .pdf) { pdf in
-            pdf.dataRepresentation() ?? Data()
-        }
-     }
 }
