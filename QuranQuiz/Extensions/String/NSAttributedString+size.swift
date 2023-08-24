6c3b2bc1db007f7432489b981208fcf0d68829bf
Takasur Azeem <takasurazeem@gmail.com>
2023-08-24 05:08:35 +0500

    Code cleanup.

---
+++ QuranQuiz/Extensions/String/NSAttributedString+size.swift
@@ -0,0 +1,27 @@
+//
+//  NSAttributedString+size.swift
+//  QuranQuiz
+//
+//  Created by Takasur Azeem on 24/08/2023.
+//
+
+import Foundation
+
+extension NSAttributedString {
+
+    func height(containerWidth: CGFloat) -> CGFloat {
+
+        let rect = self.boundingRect(with: CGSize.init(width: containerWidth, height: CGFloat.greatestFiniteMagnitude),
+                                     options: [.usesLineFragmentOrigin, .usesFontLeading],
+                                     context: nil)
+        return ceil(rect.size.height)
+    }
+
+    func width(containerHeight: CGFloat) -> CGFloat {
+
+        let rect = self.boundingRect(with: CGSize.init(width: CGFloat.greatestFiniteMagnitude, height: containerHeight),
+                                     options: [.usesLineFragmentOrigin, .usesFontLeading],
+                                     context: nil)
+        return ceil(rect.size.width)
+    }
+}
---
+++ QuranQuiz/Extensions/String/String+size.swift
@@ -0,0 +1,46 @@
+//
+//  String+size.swift
+//  QuranQuiz
+//
+//  Created by Takasur Azeem on 24/08/2023.
+//
+
+import Foundation
+
+extension String {
+
+    func width(usingFont font: UIFont) -> CGFloat {
+        let fontAttributes = [NSAttributedString.Key.font: font]
+        let size = self.size(withAttributes: fontAttributes)
+        return size.width
+    }
+
+    func heightOfString(usingFont font: UIFont) -> CGFloat {
+        let fontAttributes = [NSAttributedString.Key.font: font]
+        let size = self.size(withAttributes: fontAttributes)
+        return size.height
+    }
+
+    func sizeOfString(usingFont font: UIFont) -> CGSize {
+        let fontAttributes = [NSAttributedString.Key.font: font]
+        return self.size(withAttributes: fontAttributes)
+    }
+    
+    func size(font: UIFont, width: CGFloat) -> CGSize {
+        let attrString = NSAttributedString(
+            string: self,
+            attributes: [NSAttributedString.Key.font: font]
+        )
+        let bounds = attrString.boundingRect(
+            with: CGSize(
+                width: width, height: .greatestFiniteMagnitude
+            ),
+            options: .usesLineFragmentOrigin, context: nil
+        )
+        let size = CGSize(
+            width: bounds.width,
+            height: bounds.height
+        )
+        return size
+    }
+}
