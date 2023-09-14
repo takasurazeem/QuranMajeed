66644f40740a36cb34b4ee905e77bad63d27bcaf
Takasur Azeem <takasurazeem@gmail.com>
2023-09-15 03:52:35 +0500

    Update QuizViewModel.swift

--- QuranMajeed/Quiz/Main/ViewModel/QuizViewModel.swift
+++ QuranMajeed/Quiz/Main/ViewModel/QuizViewModel.swift
@@ -23,12 +23,13 @@ extension QuizView {
             }
             setTextForSelectedAya()
             
+            // For testing only.
 //            selectedVerses = surahs[4].verses[1...5].map { verse in
 //                QuizVerse(surahId: surahs[4].id, ayahId: verse.id, text: verse.text, translation: verse.translation)
 //            }
-            selectedVerses = surahs[0].verses.map { verse in
-                QuizVerse(surahId: surahs[4].id, ayahId: verse.id, text: verse.text, translation: verse.translation)
-            }
+//            selectedVerses = surahs[0].verses.map { verse in
+//                QuizVerse(surahId: surahs[4].id, ayahId: verse.id, text: verse.text, translation: verse.translation)
+//            }
         }
         
         func setTextForSelectedAya() {
