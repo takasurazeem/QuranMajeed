757d6bc4e5f5cfa808adaa3e73a510643835ab6a
Takasur Azeem <takasurazeem@gmail.com>
2023-09-26 05:42:09 +0500

    Update ChipContainerView.swift
    For better preview.

--- QuranMajeed/Quiz/CustomComponents/ChipView/ChipContainerView.swift
+++ QuranMajeed/Quiz/CustomComponents/ChipView/ChipContainerView.swift
@@ -50,15 +50,34 @@ struct ChipContainerView: View {
 }
 
 struct ChipContainerView_Previews: PreviewProvider {
+    static var exampleWords: [ChipModel] {
+        var words = Set<WordForWordsMeaning>()
+        for (lineIndex, line) in ipsum.split(separator: ".").enumerated() {
+            for (index, word) in line.split(separator: " ").enumerated() {
+                words
+                    .insert(
+                        WordForWordsMeaning(
+                            id: Double(lineIndex) + (Double("0.\(index)") ?? 0.0),
+                            word: String(word)
+                        )
+                    )
+            }
+        }
+        return Array(words)
+            .sorted()
+            .map(ChipModel.init)
+    }
     static var previews: some View {
-        ChipContainerView(
-            viewModel: ChipsViewModel(
-                chipArray: []
+            ChipContainerView(
+                viewModel: ChipsViewModel(
+                    chipArray: exampleWords
+                )
             )
-        )
     }
 }
 
+
+fileprivate let ipsum = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."
 
 
 
