b6f219d3c69b5d3a78d46fa2f44617f40a692256
Takasur Azeem <takasurazeem@gmail.com>
2023-10-22 05:35:18 +0500

    Add Fontblaster package to handle fonts.

--- QuranMajeed/QuranQuizApp.swift
+++ QuranMajeed/QuranQuizApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import FontBlaster
 
 @main
 struct QuranMajeedApp: App {
+    
+    init() {
+        FontBlaster.blast() // Defaults to Bundle.main if no arguments are passed
+    }
+    
     var body: some Scene {
         WindowGroup {
             QuizView(
