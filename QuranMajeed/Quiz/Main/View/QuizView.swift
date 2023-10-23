246b2722753d9311f7568d26c2b7d366ee461194
Takasur Azeem <takasurazeem@gmail.com>
2023-10-23 10:56:42 +0500

    Update QuizView.swift
    Remove the print statement.

--- QuranMajeed/Quiz/Main/View/QuizView.swift
+++ QuranMajeed/Quiz/Main/View/QuizView.swift
@@ -46,7 +46,6 @@ struct QuizView: View {
                 }
             }
             .task {
-                print(appLocale.identifier)
                 await viewModel.start()
             }
         }
